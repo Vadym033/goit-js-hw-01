77cdf8939ba093437787c95778363497ab4219fd
Vadym033 <150912531+Vadym033@users.noreply.github.com>
2024-02-26 21:51:35 +0100

    home work finish

--- js/task-1.js
+++ js/task-1.js
@@ -1,23 +1,10 @@
 'use strict';
 
 function makeTransaction(quantity, pricePerDroid) {
-  let totalPrice = quantity * pricePerDroid;
-  console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
+  const totalPrice = quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
-let quantity = 5;
-let pricePerDroid = 3000;
-makeTransaction(quantity, pricePerDroid);
-
-quantity = 3;
-pricePerDroid = 1000;
-makeTransaction(quantity, pricePerDroid);
-
-quantity = 10;
-pricePerDroid = 500;
-makeTransaction(quantity, pricePerDroid);
-
-/* 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!" */
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
--- js/task-2.js
+++ js/task-2.js
@@ -2,24 +2,9 @@
 
 function getShippingMessage(country, price, deliveryFee) {
   let totalPrice = price + deliveryFee;
-  console.log(`Shipping to ${country} will cost ${totalPrice} credits`);
+  return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
 
-let country = 'Australia';
-let price = 120;
-let deliveryFee = 50;
-getShippingMessage(country, price, deliveryFee);
-
-country = 'Germany';
-price = 80;
-deliveryFee = 20;
-getShippingMessage(country, price, deliveryFee);
-
-country = 'Sweden';
-price = 100;
-deliveryFee = 20;
-getShippingMessage(country, price, deliveryFee);
-
-/* console.log(getShippingMessage('Australia', 120, 50)); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingMessage('Australia', 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage('Germany', 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage('Sweden', 100, 20)); // "Shipping to Sweden will cost 120 credits" */
+console.log(getShippingMessage('Sweden', 100, 20)); // "Shipping to Sweden will cost 120 credits"
